11bf1b37e99e7cf351c1bd43142f9e328140b6ae
kim-yoonseung <yoonseung.kim@sf.snu.ac.kr>
2015-09-14 17:27:51 +0900

    Update solution.rkt

--- lab/20150914/solution.rkt
+++ lab/20150914/solution.rkt
@@ -15,7 +15,7 @@
 (define (maxima lst)
   (if (equal? lst nil)
       0
-      (max (car lst) (maxima (cdr list)))))
+      (max (car lst) (maxima (cdr lst)))))
 (maxima '()) ; 0
 (maxima '(1 5 2 4 3)) ; 5
 (maxima '(7 3 1)) ; 7
